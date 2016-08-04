96ce72f5dbd6e7c89debeb6ac75888d812d80c0c
Willy Rivera <riverawilly@gmail.com>
2016-08-04 12:15:17 -0430

    Most functions

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -5,6 +5,10 @@ class Api extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->helper('file');
+		$check = read_file('./application/data/data.txt');
+		echo $check;
+		die;
 		$data = array();
 		$data['form_login'] = $this->html->GetLogin();
 		$data['domain'] = 'http://localhost/system';
